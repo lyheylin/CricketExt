75dcdeb864a867ce944f6398ebb9bc67857cbfe9
eyhlin <eylin@iastate.edu>
2024-04-26 15:42:10 +0800

    Simple restructure, testing reading video file.

--- CricketExt.cs
+++ CricketExt.cs
@@ -5,24 +5,53 @@ using OpenCvSharp;
 namespace CricketExt {
     public class CricketExt {
         static async Task<int> Main(String[] args) {
-            Debug.WriteLine($"Start program");
-
-            // Command line parsing
-            var file = new Option<FileInfo?>(
-                name: "--file",
-                description: "Video file to analyze.");
-            var rootCommand = new RootCommand("Capture screenshots from a video and read the data on the scoreboard shown in the video.");
-
-            rootCommand.AddOption(file);
-            rootCommand.SetHandler(f => { ReadFile(f!); }, file);
-
+            // Command line
+            RootCommand rootCommand = ParseCL(args);
             await rootCommand.InvokeAsync(args);
 
             return 0; 
         }
 
+        static RootCommand ParseCL(String[] args) {
+            var file = new Option<FileInfo?>(
+               name: "--file",
+               description: "Video file to analyze.");
+            var rootCommand = new RootCommand("Capture screenshots from a video and read the data on the scoreboard shown in the video.");
+
+            rootCommand.AddOption(file);
+            //Bind handlers
+            rootCommand.SetHandler(f => { ReadFile(f!); }, file);
+
+            return rootCommand; 
+        }
+
         static void ReadFile(FileInfo file) {
             Debug.WriteLine($"Reading File: {file.Name}");
+            String filePath = file.FullName;
+            VideoCapture video = new VideoCapture(filePath);
+            
+            if (!video.IsOpened()) 
+                Debug.WriteLine($"err: Can not open: {filePath}");
+
+            while (video.IsOpened()) {
+                Mat frame;
+                frame = new Mat(video.FrameHeight, video.FrameWidth, MatType.CV_8UC3);
+                bool next = video.Read(frame);
+                if (next)
+                    Cv2.ImShow("Display", frame);
+                else
+                    Debug.WriteLine("End of video");
+
+
+                int key = Cv2.WaitKey(0);
+                if ((key & 0xFF) == Convert.ToUInt32('q'))
+                    break;
+                
+            }
+
+            video.Release();
+            Cv2.DestroyAllWindows();//debug;move
         }
     }
 }   
+
